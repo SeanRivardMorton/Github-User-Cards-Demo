55975c94991d468b64b816852e8398fbc45ba6e3
SeanRivardMorton <sean.rivard.morton@gmail.com>
2018-06-04 01:33:32 -0400

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   resources :gitusers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#gitusers'
+  root 'pages#home'
 end
