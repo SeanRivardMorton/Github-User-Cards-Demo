fa7181841d34bc7a18050ee34568dfbed8721343
SeanRivardMorton <sean.rivard.morton@gmail.com>
2018-06-13 23:45:31 -0400

    fixed type & changed strong to h1

--- app/javascript/packs/components/GitHubBrowser/GitHubUserCard.vue
+++ app/javascript/packs/components/GitHubBrowser/GitHubUserCard.vue
@@ -1,16 +1,14 @@
 <template>
-    <div>
-        <div v-for="user in users" :key="user.id" class="gituser__card">
-            <img class="gituser__card__img" :src="user.avatar_url" alt="">
-            <div class="gituser__card__description">
-                <span>
-                    <strong>{{ user.name }}</strong>
-                    <span class="gituser__card__time"> 7 days </span>
-                </span>
-                <p v-html="user.description"></p>
-            </div>
-        </div>
+  <section>
+    <div v-for="user in users" :key="user.id" class="gituser__card">
+      <img class="gituser__card__img" :src="user.avatar_url" alt="">
+      <div class="gituser__card__description">
+        <h1>{{ user.name }}</h1>
+        <span class="gituser__card__time"> 7 days </span>
+        <p v-html="user.description"></p>
+      </div>
     </div>
+  </section>
 </template>
 
 <script>
@@ -22,7 +20,7 @@ export default {
         {
           name: 'horse_js',
           description:
-            'as much as I want to be, I am not <a href="www.google.com">test</a>',
+            'as much as I want to be, I am not <a href="www.google.com">@horse_js</a>',
           avatar_url:
             'https://pbs.twimg.com/profile_images/1844491454/horse-js_400x400.png'
         }
@@ -39,6 +37,7 @@ export default {
   box-shadow: 0px 0px 1px 0px #dcd9d9;
   display: flex;
   box-shadow: 0px 2px 20px 0px #0000006b;
+  border-radius: 1px;
 }
 
 .gituser__card__img {
@@ -52,6 +51,10 @@ export default {
   margin: 0 auto;
   p {
     margin: 1px 0 0 0;
+  }
+  h1 {
+    font-size: 1rem;
+    display: contents;
   }
 }
 
